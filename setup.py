628e95b787f59776a8ef1709dbe0ccae450f55f0
sieun-hcs <sieun.park@hcs.snu.ac.kr>
2026-03-03 17:13:40 +0900

    Add setup.py from meta-llama/llama

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-successfully downloaded text file (SHA: 57f86dcbb4bded68a5f9ce318241dfb7ccb5b96d)
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+from setuptools import find_packages, setup
+
+
+def get_requirements(path: str):
+    return [l.strip() for l in open(path)]
+
+
+setup(
+    name="llama",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
